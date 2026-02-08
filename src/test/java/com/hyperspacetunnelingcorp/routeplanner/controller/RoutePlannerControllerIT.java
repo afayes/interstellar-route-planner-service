a7adb554963610e84be28a7c1447025e1889dfe9
afayes <afayes@hotmail.co.uk>
2026-02-08 09:54:17 +0000

    RoutePlannerControllerIT tests

--- src/test/java/com/hyperspacetunnelingcorp/routeplanner/controller/RoutePlannerControllerIT.java
+++ src/test/java/com/hyperspacetunnelingcorp/routeplanner/controller/RoutePlannerControllerIT.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -28,6 +29,7 @@ import com.hyperspacetunnelingcorp.routeplanner.model.Transport;
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class RoutePlannerControllerIT {
 
     @Autowired
